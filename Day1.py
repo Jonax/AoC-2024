e2eb43d735e1d711f8dbcc7144bfe6716f6467b9
Jon Wills <git@goldenwolf.net>
2024-12-08 16:53:56 +0000

    [Day 1] Overhauled function
    Cleanup of approach, and merging of the two parts into one solution.

--- Day1.py
+++ Day1.py
@@ -1,35 +1,48 @@
 import os
+import re
 from collections import Counter
 
+'''
+APPROACH:
+	Parse the input into two columns of files.
+
+	For Part 1:
+		Sort both lines in ascending order. 
+		Find the absolute distance between each sorted row. 
+		Sum up all the differences.
+
+	For Part 2:
+		Count number of instances of each unique number on right.
+		For each number on left, multipy value by number of its occurance on right.
+		Sum up all the results.
+'''
+
 def Parse(inputFile):
-	a, b = [], []
+	inputRegex = re.compile(r"(\d+)\s+(\d+)")
 
 	with open(inputFile) as inFile:
-		for line in inFile.readlines():
-			line = line.strip().partition("   ")
-			
-			a.append(int(line[0]))
-			b.append(int(line[-1]))
+		for line in inputRegex.findall(inFile.read()):
+			assert len(line) == 2
+			yield tuple(int(x) for x in line)
 
-	return a, b
+def Solve(inputFile, combine = False):
+	lines = list(Parse(inputFile))
 
-def PartA(inputFile):
-	left, right = Parse(inputFile)
+	left = [x[0] for x in lines]
+	if not combine:
+		# Part 1
+		left.sort()
+		right = sorted(x[1] for x in lines)
 
-	left.sort()
-	right.sort()
+		return sum(abs(l - r) for l,r in zip(left, right))
 
-	return sum(abs(l - r) for l,r in zip(left, right))
-
-def PartB(inputFile):
-	left, right = Parse(inputFile)
-
-	right = Counter(right)
+	# Part 2
+	right = Counter(x[1] for x in lines)
 
 	return sum([l * right[l] for l in left])
 
-assert PartA("Day1_Example.txt") == 11
-assert PartA("Day1_Input.txt") == 1941353
+assert Solve("Day1_Example.txt") == 11
+assert Solve("Day1_Input.txt") == 1941353
 
-assert PartB("Day1_Example.txt") == 31
-print(PartB("Day1_Input.txt"))
+assert Solve("Day1_Example.txt", combine = True) == 31
+assert Solve("Day1_Input.txt", combine = True) == 22539317
