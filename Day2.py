9f80157e579f372022488b01261ffa22fdc6c192
Jon Wills <git@goldenwolf.net>
2024-12-08 17:59:01 +0000

    [Day2] Script overhaul
    Commenting, Ruff linting, explanations, etc

--- Day2.py
+++ Day2.py
@@ -1,40 +1,80 @@
-import os
-from collections import Counter
+'''
+APPROACH:
+	Parse the input into lines of integer sequences.  
+	Evaluate each report's safety by the number of "errors" in the sequence.
+	An error is determined as when the current value and the last "good" value 
+	don't follow the rules 
+
+	For Part 1:
+		Any errors at all in a report means the report can be disregarded.
+		Therefore, only count those where numErrors = 0
+
+	For Part 2:
+		One error is tolerable, as long as the following value works with the 
+		last good value.
+		Therefore, only count those where numErrors = 0 or 1.
+'''
 
 def Parse(inputFile):
 	with open(inputFile) as inFile:
 		for line in inFile.readlines():
 			yield [int(x) for x in line.strip().split(" ")]
 
+# Rate a report's "safety" based on the number of errors found.
 def GetReportSafety(line):
+	# Identify which direction the report is expected to be going using
+	# the first two values.
+	# If any deltas along the same sequence go in the opposite direction, 
+	# that suggests a switched direction somewhere and therefore an error.
 	direction = line[1] - line[0]
 
-	numDangers = 0
-	lastGoodValue = line[0]
+	# Error counter
+	numErrors = 0
+
+	# Tracks the last value that passed, for comparing later values against.
+	# Starts as the first value in the list, since there's nothing before it to
+	# contradict.
+	lastValidValue = line[0]
+
+	# Start at the second value so we can start comparing against the 
+	# first value. 
 	for current in line[1:]:
-		delta = current - lastGoodValue
+		# Find the delta from last valid to current.
+		delta = current - lastValidValue
 
+		# If the delta's zero, the two numbers match which should not happen.
 		if delta == 0:
-			numDangers += 1
+			numErrors += 1
 			continue
 
+		# If multiplying delta by the original direction results in a 
+		# negative number, the delta's going in the opposite direction
+		# (similar to using dot product). Ergo, an error.  
 		if direction * delta < 0:
-			numDangers += 1
+			numErrors += 1
 			continue
 
+		# Finally, if the absolute delta is not between 1 & 3, then it's
+		# out of range and therefore an error.  
 		if not (1 <= abs(delta) <= 3):
-			numDangers += 1
+			numErrors += 1
 			continue
 
-		lastGoodValue = current
+		# If reaching this point, the value was good so make it the 
+		# next "good" value.  
+		lastValidValue = current
 
-	return numDangers
+	# In the end, return however many errors were encountered.  
+	return numErrors
 
-def Solve(inputFile, maxTolerance):
+def Solve(inputFile, maxTolerance = 0):
+	# Return the number of reports that had at most the number of 
+	# errors deemed acceptable.  
 	return sum(GetReportSafety(report) <= maxTolerance for report in Parse(inputFile))
 
-assert Solve("Day2_Example.txt", 0) == 2
-assert Solve("Day2_Input.txt", 0) == 483
+if __name__ == "__main__":
+	assert Solve("Day2_Example.txt") == 2
+	assert Solve("Day2_Input.txt") == 483
 
-assert Solve("Day2_Example.txt", 1) == 4
-assert Solve("Day2_Input.txt", 1) == 528
+	assert Solve("Day2_Example.txt", maxTolerance = 1) == 4
+	assert Solve("Day2_Input.txt", maxTolerance = 1) == 528
