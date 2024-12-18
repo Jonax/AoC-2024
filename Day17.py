16a70bba2d473ef70cb5d6e364ac0fe2541b1462
Jon Wills <git@goldenwolf.net>
2024-12-18 02:16:23 +0000

    [Day 17] Documented solution

--- Day17.py
+++ Day17.py
@@ -1,6 +1,45 @@
 import pytest
 import re
 
+'''
+	APPROACH:
+		The problem describes a LOLCODE machine in sufficient detail, mentioning what each opcode 
+		should do when encountered.
+
+		Parse the input into a program consisting of initial values for three registers (A, B, C) and
+		a series of 3-bit "instructions" (0-7).
+		Place a caret at the start of the instructions.
+		For each step, parse the number the caret is pointing to as its opcode, and the following 
+		number as its literal operand.  
+			- Actions to take wholly depend on the opcode value, and are too complex to detail here. 
+			- Depending on the opcode, the literal operand may be used directly, or may need to be used
+			  to derive the combo operand for use instead.  
+			- The caret will normally increment by two after each step, but if a JNZ operation then 
+			  special logic will override this.  
+			- Reduced to their simplest, almost all of the operations are bitwise operations. 
+		Repeat until the program reaches the end of its instructions. 
+		In the process, a series of 3-bit values should be built up as output.  
+
+		For Part 1:
+			The program is run once. 
+			Parse the program, run it and collect its output.
+			Return output as a comma-delimited string.  
+
+		For Part 2:
+			Parse the program and store as a template. 
+			Set n = 0.
+			Start from the end of the instructions. 
+			For each instruction (in reverse):
+				- Derive a reference list from the instruction to the end.
+				- Bit-shift n three bits to the left.
+				- While incrementing n:
+					- Calculate the resulting program for n.
+					- Once the program matches the reference, stop and continue for the next instruction.
+				- Don't reset n between instructions.
+			Once all instructions have been processed, n should be the lowest n required for recreating 
+			the original program string.
+'''
+
 def Parse(inputFile):
 	programRegex = re.compile(r"Register A: (?P<a>\d+)\nRegister B: (?P<b>\d+)\nRegister C: (?P<c>\d+)\n\nProgram: (?P<prog>[\d,]+)")
 
@@ -18,10 +57,18 @@ def Parse(inputFile):
 		}
 
 def GetComboOperand(registers, operand):
+	# Uses the literal operand to determine the value of the combo operand, as required by some
+	# operations.  
+
+	# [0-3] Use operand as the literal value.  
 	if 0 <= operand <= 3:
 		return operand
 
+	# [7] Unused, so this should never trigger.
 	assert operand <= 6
+
+	# [4-6] Use the value of the respective register.
+	# 4 = A, 5 = B, 6 = C
 	return registers[operand - 4]
 
 def GenerateProgram(payload, registers = None):
@@ -56,6 +103,8 @@ def GenerateProgram(payload, registers = None):
 		elif opcode == 4: # bxc (unused)
 			registers[1] ^= registers[2]
 		elif opcode == 5: # out (combo)
+			# Yielding rather than adding to a local list gives us more power on how to use the result
+			# (namely, short-circuiting a program we know won't be what we want. 
 			yield GetComboOperand(registers, operand) % 8
 		elif opcode == 6: # bdv (combo)
 			registers[1] = registers[0] >> GetComboOperand(registers, operand)
@@ -72,6 +121,13 @@ def PartB(inputFile):
 
 	n = 0
 	for i in range(len(payload["program"]), 0, -1):
+		# Observed when running the test & input cases manually is that the length of the resulting 
+		# program increases in length for every base-8 OOM. We can use this quirk to skip up huge 
+		# swathes of values of n by bit-shifting n three bits to the left each time the result matches
+		# the end of the reference program.  
+		# We do it at the start of the loop because doing it at the end would cause one more bit-shift 
+		# than needed and provide a wrong answer. Instead, 0 will be bit-shifted to 0 at the start, 
+		# having no adverse effect.  
 		n <<= 3
 
 		reference = payload["program"][i - 1:]
